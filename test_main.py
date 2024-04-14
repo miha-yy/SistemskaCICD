6d08984cd7d7868e2bd9c7b61d38614bb3a6b1f5
miha-yy <mojkakec12345@gmail.com>
2024-04-14 14:51:31 +0200

    update test

--- test_main.py
+++ test_main.py
@@ -5,10 +5,10 @@ def test_calculate_fibonacci():
     tests = [(1, 1), (2, 1), (3, 2), (4, 3), (5, 5), (6, 8), (7, 13), (8, 21), (9, 34), (10, 55)]
     for n, expected_fibonacci_result in tests:
         fibonacci_result = calculate_fibonacci(n)
-        assert fibonacci_result == expected_fibonacci_result, f"The {n}th Fibonacci number should be {expected_fibonacci_result}, but got {fibonacci_result}"
+        assert fibonacci_result == expected_fibonacci_result, f"The {n}th Fibonacci number should be {expected_fibonacci_result}, got {fibonacci_result}"
 
 def test_calculate_lucas():
     tests = [(1, 1), (2, 3), (3, 4), (4, 7), (5, 11), (6, 18), (7, 29), (8, 47), (9, 76), (10, 123)]
     for n, expected_lucas_result in tests:
         lucas_result = calculate_lucas(n)
-        assert lucas_result == expected_lucas_result, f"The {n}th Lucas number should be {expected_lucas_result}, but got {lucas_result}"
+        assert lucas_result == expected_lucas_result, f"The {n}th Lucas number should be {expected_lucas_result}, got {lucas_result}"
