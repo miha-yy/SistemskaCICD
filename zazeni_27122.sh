d54052707291298ff8d33a9722b7ff69280e879d
miha-yy <mojkakec12345@gmail.com>
2024-04-14 20:13:41 +0200

    updaate script

--- zazeni_27122.sh
+++ zazeni_27122.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-USERNAME="mihayy9"
-REPO_NAME="sistemska"
-TAG="latest"
+docker pull mihayy9/sistemska:latest
 
-docker pull $USERNAME/$REPO_NAME:$TAG
+docker run -d --name cicd mihayy9/sistemska:latest
 
-docker run -d --name sistemska_container $USERNAME/$REPO_NAME:$TAG
-
-echo "Container sistemska_container is running using image $USERNAME/$REPO_NAME:$TAG"
+echo "Container cicd is running using image mihayy9/sistemska:latest"
